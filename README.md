ab18c1ffed1f161f0f50859b23fb483b0479e8a4
Ritesh <Riteshk229@gmail.com>
2023-03-22 04:06:21 +0530

    g

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Alarm-Clock
+# Alarm-Clock.github.io
+
+In this Project we were given the task of creating an Alarm Clock and set an alarm to it and we were also told to make a list of alarm and show it too.
+
+## index.html
+In the html file you will see that the page consist of a main section which contains the Alarm Clock Container. This contaier is divided into three section containing display section to show current time, set alarm section and alarm list section.
+
+## style.css
+The style.css file contains code by which the page is styled.
+
+
+## script.js
+The script.js contains the dynamic code of the project. It contains function like crtime() which updated time every sec, addAlarm() to set the alarm provided by the users, showAlarm() to display the list of alarm in the Upcoming Alarm Section and remove() to remove a desired alarm.
+
+
+## live demonstration of the project can viewed via this link https://riteshk229.github.io/Alarm-Clock.github.io/ .
---
+++ script.js
@@ -0,0 +1,131 @@
+
+//List to show the stored alarm
+var myList = document.querySelector('#myList');
+//new Alarm
+var addAlarm = document.querySelector('.setAlarm');
+
+//List to store the alarms
+var alarmList = [];
+
+
+//Alarm Notification
+function AlarmRinging(h2){
+    alert(`Hey the Time is ${h2}`);
+}
+
+
+//To display current time
+var h2 = document.getElementById('clock');
+
+//To add zero in-front if required
+function zeroPad(num) {
+    var length = num.toString().length;
+    if(length <= 2){
+        return num.toString().padStart(2, "0");
+    }
+    else{
+        return num
+    }
+  }
+
+//Time Function
+function crtime(){
+
+    var now = new Date();
+    var hours;
+    var minutes = now.getMinutes();
+    var sec = now.getSeconds();
+
+    var ampm = (now.getHours()) < 12 ? "AM" : "PM";
+    
+    if(now.getHours()  <= 12 ){
+        hours = now.getHours();
+    }
+    else {
+        hours = now.getHours() - 12;
+    }
+
+    h2.innerText = zeroPad(hours) + ":" +  zeroPad(minutes) + ":" + zeroPad(sec) + " " + ampm;
+
+    now = h2.innerText;
+
+    //Checking if the alarm time matches with the current time
+    if(alarmList.includes(now) ){
+        AlarmRinging(now);
+    } 
+}
+
+//Current Time ticker
+var currentTime = setInterval(crtime,1000);
+
+
+
+//To set alarm
+function setAlarm(){
+       
+}
+
+
+//New Alarm 
+addAlarm.addEventListener('submit', e =>{
+    //Stops the page from Reloading
+    e.preventDefault();
+    
+    var new_h = zeroPad(addAlarm.alh.value);
+    var new_m = zeroPad(addAlarm.alm.value);
+    var new_s = zeroPad(addAlarm.als.value);
+    var ampm = addAlarm.ampm.value;
+    
+    var newAlarm = `${new_h}:${new_m}:${new_s} ${ampm}`;
+
+    //Add newAlarm to alarmList
+    if(isNaN(newAlarm)){
+        if(!alarmList.includes(newAlarm)){
+            alarmList.push(newAlarm);
+            console.log(alarmList);
+            console.log(alarmList.length);
+            showNewAlarm(newAlarm);
+            addAlarm.reset();
+        } else{
+            alert(`Alarm for ${newAlarm} already set.`);
+        }
+    } else{
+        alert("Invalid Time Entered")
+    }     
+});
+
+//Function to display the list of alarm
+function showNewAlarm(newAlarm){
+    const html =`
+    <li class = "time-list"> 
+        <span class="time">${newAlarm}</span>
+        <button class="deleteAlarm time-control" id="delete-button" value=${newAlarm} onclick = "removeAlrm(this.value)">Delete Alarm</button>          
+    </li>`
+    myList.innerHTML += html
+};
+
+
+//To remove the deleted Alarm 
+myList.addEventListener('click', e=> {
+    // console.log("removing element")
+    if(e.target.classList.contains("deleteAlarm")){
+        e.target.parentElement.remove();
+    }    
+})
+
+
+//Removes an alarm from the array when "Delete Alarm" is clicked
+function removeAlrm(value) {
+    let newList = [];
+
+    for(var i = 0; i <alarmList.length ;i++){
+        if(!alarmList[i].includes(value)){
+           newList.push(alarmList[i]);
+        }
+    }
+
+    alarmList=newList;
+    
+    // console.log("newList", newList);
+    // console.log("alarmList", alarmList);
+}
